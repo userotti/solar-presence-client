fbe3536d354885840e9091421fe01b9473b146eb
Christian Frauenstein <cfrauenstein@gmail.com>
2017-04-03 10:58:03 +0200

    Initial setup stuff, yarn, gitignore

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by christian on 2017/04/03.
+ */
+console.log("hello world");
+const color = require('color')
+const redHexa = color({ r: 255, g: 0, b: 0 }).hex()
+console.log(redHexa)
